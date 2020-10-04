b2cfbf263f6e1e25f59804a9a80a983a65abf0c5
Lopatovas <lopatovas@gmail.com>
2020-10-04 21:40:05 +0300

    Add leftover files

--- CvApi/Services/JobAdvertisementService/JobAdvertisementService.cs
+++ CvApi/Services/JobAdvertisementService/JobAdvertisementService.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using CvApi.Models.Entities.ResolvingTables;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CvApi.Services.JobAdvertisementService
 {
-    public class JobAdvertisementService
+    public class JobAdvertisementService : IJobAdvertisementService
     {
+        public void CreateAdvertisement(JobAdvertisement advertisement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAdvertisement(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JobAdvertisement GetAdvertisementById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<JobAdvertisement> GetAdvertisements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAdvertisement(Guid id, JobAdvertisement advertisement)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
